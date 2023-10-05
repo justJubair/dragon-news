73ca46bf0e4cbd9fb405c1fe29a1dcb4aba3a710
Jubair <jubair.ahmed2838@gmail.com>
2023-10-05 15:00:45 +0600

    Add: updated firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -3,14 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBvVcCNMW3isbv2Ko7SDKhxiJBPleDsLxQ",
-  authDomain: "dragon-news-4ea04.firebaseapp.com",
-  projectId: "dragon-news-4ea04",
-  storageBucket: "dragon-news-4ea04.appspot.com",
-  messagingSenderId: "844827707245",
-  appId: "1:844827707245:web:8c60677c477f5c0f197c64"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
-
+ 
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,11 +1,46 @@
-
-
+import Navbar from "../../components/Navbar/Navbar";
 const About = () => {
   return (
     <div>
-      <h1>this is about page</h1>
+      <Navbar />
+      <div className="flex flex-col justify-between mt-12 md:flex-row">
+        <div>
+          <h1 className="font-semibold text-xl mb-4" >Frequently Asked Questions</h1>
+          <div className="collapse collapse-arrow bg-base-200">
+            <input type="radio" name="my-accordion-2" checked="checked" />
+            <div className="collapse-title text-xl font-medium">
+              Click to open this one and close others
+            </div>
+            <div className="collapse-content">
+              <p>hello</p>
+            </div>
+          </div>
+          <div className="collapse collapse-arrow bg-base-200 my-6">
+            <input type="radio" name="my-accordion-2" />
+            <div className="collapse-title text-xl font-medium">
+              Click to open this one and close others
+            </div>
+            <div className="collapse-content">
+              <p>hello</p>
+            </div>
+          </div>
+          <div className="collapse collapse-arrow bg-base-200">
+            <input type="radio" name="my-accordion-2" />
+            <div className="collapse-title text-xl font-medium">
+              Click to open this one and close others
+            </div>
+            <div className="collapse-content">
+              <p>hello</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex-1 text-center px-4">
+          <h1 className="text-2xl font-bold mb-4" >About Us</h1>
+          <p className="text-start text-slate-500 px-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure voluptatem officia commodi deleniti, cum optio esse, deserunt numquam voluptates minima expedita dolorum et ullam eius mollitia earum aspernatur placeat quod! Asperiores, velit? Itaque quos voluptate explicabo exercitationem ipsa voluptatibus assumenda similique numquam, dicta tempora blanditiis praesentium quas ab distinctio ex fugit consectetur quaerat hic! Nam obcaecati vero earum accusamus repudiandae, reprehenderit aliquam asperiores voluptatum expedita consequuntur velit perspiciatis praesentium in quia consequatur debitis! Cumque in saepe deleniti aliquid aperiam cupiditate veniam, suscipit voluptas vero facere officia et commodi ab placeat quidem adipisci enim! Quo tempore itaque, eaque animi sed asperiores..</p>
+        </div>
+      </div>
     </div>
-  )
-}
- 
-export default About
+  );
+};
+
+export default About;
